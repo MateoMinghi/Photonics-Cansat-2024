54b65d92a369e3b414bc88c71d36178c4eba2cf9
Mateo Minghi <59071760+MateoMinghi@users.noreply.github.com>
2024-04-07 00:02:51 -0600

    Update CSV_approach.py

--- Visualization/CSV_approach.py
+++ Visualization/CSV_approach.py
@@ -22,3 +22,22 @@ while True:
     # Wait for some time before collecting more data
     time.sleep(1)  # Collect data every 1 second
 #this would be a separate script from the visualization one
+
+
+#in the visualization file:
+
+'''
+def update_visualization():
+    while True:
+        # Read CSV file
+        data = pd.read_csv(csv_file)
+        
+        # Create Panel plot
+        plot_object = data.hvplot(x='time', y=['sensor1', 'sensor2'], title='Live Sensor Data')
+        
+        # Update Panel app
+        app[:] = [plot_object]
+        
+        # Wait for some time before updating visualization
+        time.sleep(5)  # Update visualization every 5 seconds
+'''
